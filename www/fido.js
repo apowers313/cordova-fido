a3121c2eac94697a878087d8ec3ee2511890d925
Adam Powers <apowers@ato.ms>
2016-10-20 15:00:34 -0700

    added real API

--- www/fido.js
+++ www/fido.js
@@ -1,7 +1,46 @@
 var exec = require('cordova/exec');
 
-exports.uafDiscover = function(arg0, success, error) {
-    console.log ("uafDiscover in fido.js");
-    exec(success, error, "fido", "uafDiscover", [arg0]);
-    console.log ("uafDiscover done.");
+/**
+ * These functions are basically convenience functions for cordova.exec()
+ * They bind to the 'clobbers' target defined in the plugin.xml file, such that
+ * uafDiscover() becomes fido.uafDiscover() in the Cordova application
+ */ 
+var fido = {
+    uafDiscover: function() {
+        return new Promise(function(resolve, reject) {
+            exec(
+                function(ret) { // success
+                    resolve (ret);
+                },
+                function(err) { // error
+                    reject (err);
+                }, "fido", "uafDiscover", []);
+        });
+    },
+
+    uafCheckPolicy: function(message, origin) {
+        return new Promise(function(resolve, reject) {
+            exec(
+                function(ret) { // success
+                    resolve (ret);
+                },
+                function(err) { // error
+                    reject (err);
+                }, "fido", "uafCheckPolicy", [message, origin]);
+        });
+    },
+
+    uafOperation: function(message, channelBindings, origin) {
+        return new Promise(function(resolve, reject) {
+            exec(
+                function(ret) { // success
+                    resolve (ret);
+                },
+                function(err) { // error
+                    reject (err);
+                }, "fido", "uafOperation", [message, channelBindings, origin]);
+        });
+    }
 };
+
+module.exports = fido;
